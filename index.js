e575eceb325ea13a9b9480a6aaac29304f7d7e15
유경아 <yua@yugyeong-aui-MacBookAir.local>
2023-03-24 14:25:22 +0900

    mongoDB failover code 100%

--- index.js
+++ index.js
@@ -50,46 +50,100 @@ const conn = async (index) => {
       serverSelectionTimeoutMS: 2000, // server select timeout limit
       directConnection: true,
     })
-      .then((client) => {
+      .then(async (client) => {
         // connectoin success mongoDB
         console.log(
           `\n<< Success MongoDB Connection : ${replicaSet[index]} >>`
         );
 
-        client // excute mongosh command db.isMaster();
-          .db()
-          .command({ isMaster: 1 })
-          .then(async (res) => {
-            // console.log(res);
-            console.log(`>> ismaster \t==> ${res.ismaster}`); // replicaSet member? false :  true
-            console.log(`>> secondary \t==> ${res.secondary}`); // replicaSet member? true : undefined
-            console.log(`>> primary \t==> ${res.primary}`); // replicaSet member? mongo2:27017 : undefined
-            console.log(`>> me \t\t==> ${res.me}`); // replicaSet member? mongo1:27017 : undefined
-            console.log("<< ========================== >>");
+        const adminDB = client.db().admin();
+        let setName, version, me;
+        let memberIndex = 0;
+        let memberHosts = [];
+        let newMembers = [];
 
-            if (res.me === undefined) {
-              console.log("\n<< No ReplicaSet MongoDB>>");
-              console.log("==> Retry MongoDB Connection");
+        // db.isMaster();
+        adminDB.command({ isMaster: 1 }).then(async (res) => {
+          // console.log(res);
+          console.log(`>> ismaster \t==> ${res.ismaster}`); // replicaSet member? false :  true
+          console.log(`>> secondary \t==> ${res.secondary}`); // replicaSet member? true : undefined
+          console.log(`>> primary \t==> ${res.primary}`); // replicaSet member? mongo2:27017 : undefined
+          console.log(`>> me \t\t==> ${res.me}`); // replicaSet member? mongo1:27017 : undefined
+          console.log("<< ========================== >>");
 
-              // throw new Error("NoReplicaSet");
-              noReplHosts.push(replicaSet[index]);
-              await conn(index + 1);
-            } else if (res.ismaster) {
-              mongoClient = client; // sync => so, sometimes...client is undefined
-              console.log("\n<< Success Set Primary to MongoClient >>");
+          if (res.me === undefined) {
+            // No ReplicaSet MongoDB Server
+            console.log("\n<< No ReplicaSet MongoDB>>");
+            console.log("==> Retry MongoDB Connection");
 
-              if (noReplHosts.length) {
-                // rs.add && noReplHosts clear
+            // throw new Error("NoReplicaSet");
+            noReplHosts.push(replicaSet[index]);
+            await conn(index + 1);
+          } else if (res.ismaster) {
+            // Primary MongoDB Server
+            setName = res.setName;
+            version = res.setVersion;
+            me = res.me;
+
+            for (const host of res.hosts) {
+              newMembers.push({ _id: memberIndex, host: host });
+              memberIndex++;
+
+              if (host === me) {
+                continue;
               }
-            } else {
-              //   } else if (res.secondary) {
-              console.log("\n<< This MongoDB is Secondary >>");
-              console.log("==> Retry MongoDB Connection");
 
-              // throw new Error("Secondary");
-              await conn(index + 1);
+              memberHosts.push(host);
             }
-          });
+
+            if (noReplHosts.length) {
+              // rs.add && noReplHosts clear
+              for (const host of noReplHosts) {
+                newMembers.push({ _id: memberIndex, host: host });
+                memberIndex++;
+              }
+
+              noReplHosts = [];
+            }
+
+            console.log("\n<< Success Set New ReplicaSet Members >>");
+            console.log(newMembers);
+
+            const dropMember = {
+              dropConnections: 1,
+              hostAndPort: memberHosts,
+            };
+            const newConfig = {
+              _id: setName,
+              version: version + 1,
+              members: newMembers,
+            };
+
+            // rs.remove()
+            await adminDB.command(dropMember).then((res) => {
+              console.log("\n<< Success Drop Members >>");
+              console.log(res);
+            });
+            // rs.add() or rs.reconfig()
+            await adminDB
+              .command({ replSetReconfig: newConfig })
+              .then((res) => {
+                console.log("\n<< Success ReplicaSet Reconfig >>");
+                console.log(res);
+              });
+
+            mongoClient = client; // sync => so, sometimes...client is undefined
+            console.log("\n<< Success Set Primary to MongoClient >>");
+          } else {
+            // Secondary MongoDB Server
+            //   } else if (res.secondary) {
+            console.log("\n<< This MongoDB is Secondary >>");
+            console.log("==> Retry MongoDB Connection");
+
+            // throw new Error("Secondary");
+            await conn(index + 1);
+          }
+        });
 
         /**
          * mongosh command test
@@ -125,9 +179,12 @@ const conn = async (index) => {
   }
 };
 
-// conn(0); // try connect mongoDB replicaSet
+conn(0); // try connect mongoDB replicaSet
 
-MongoClient.connect("mongodb://127.0.0.1:27018/test", {
+// test code
+/*
+const testPrimary = "127.0.0.1:27017";
+MongoClient.connect(`mongodb://${testPrimary}/test`, {
   auth: {
     username: "root",
     password: "root!",
@@ -205,7 +262,7 @@ MongoClient.connect("mongodb://127.0.0.1:27018/test", {
     console.log(error.message);
     console.log(error.reason);
   });
-
+*/
 app.get("/read", async (req, res, next) => {
   // testing mongoDB client read
   try {
